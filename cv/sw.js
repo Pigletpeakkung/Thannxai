d3e6bd31325e24427f82c1d47f86d2b3d401aec8
Thanattsitt S. <Thanattsitt.info@yahoo.co.uk>
2025-05-21 07:40:00 +0700

    Create sw.js

---
+++ cv/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'cv-portfolio-v1';
+const urlsToCache = [
+  '/cv/',
+  '/cv/index.html',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+  'https://unpkg.com/aos@2.3.1/dist/aos.css',
+  'https://unpkg.com/aos@2.3.1/dist/aos.js'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
